218f3f07e1f23401bcc6082fd095dbfd8c2dbd11
NITHESH <112474206+nitheshd30@users.noreply.github.com>
2025-03-07 19:17:33 +0530

    Update and rename omni_Spacewar.mk to twrp_Spacewar.mk

--- twrp_Spacewar.mk
+++ twrp_Spacewar.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Spacewar device
-$(call inherit-product, device/nothing/Spacewar/device.mk)
+$(call inherit-product, device/nothing/spacewar/device.mk)
 
 PRODUCT_DEVICE := Spacewar
-PRODUCT_NAME := omni_Spacewar
+PRODUCT_NAME := twrp_Spacewar
 PRODUCT_BRAND := Nothing
 PRODUCT_MODEL := A063
 PRODUCT_MANUFACTURER := nothing
