82939500820d607186ec1d71200fe7dbcb14f016
NITHESH <112474206+nitheshd30@users.noreply.github.com>
2025-03-07 19:22:50 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Spacewar-user
-add_lunch_combo omni_Spacewar-userdebug
-add_lunch_combo omni_Spacewar-eng
+add_lunch_combo twrp_Spacewar-user
+add_lunch_combo twrp_Spacewar-userdebug
+add_lunch_combo twrp_Spacewar-eng
